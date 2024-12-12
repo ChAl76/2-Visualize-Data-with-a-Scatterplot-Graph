a3af5bbb3923b12413ba0ba0ae72254f459c5ba4
ChAl76 <chornyalexander@gmail.com>
2024-12-12 16:57:42 +0200

    add Scales and Axes, Tooltip, SVG Container

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const url =
 const margin = { top: 100, right: 40, bottom: 50, left: 60 };
 const width = 900 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
+
+// Scales and Axes
+const x = d3.scaleLinear().range([0, width]);
+const y = d3.scaleTime().range([0, height]);
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+const timeFormat = d3.timeFormat('%M:%S');
+const xAxis = d3.axisBottom(x).tickFormat(d3.format('d'));
+const yAxis = d3.axisLeft(y).tickFormat(timeFormat);
+
+// Tooltip setup
+const tooltip = d3
+  .select('body')
+  .append('div')
+  .attr('class', 'tooltip')
+  .attr('id', 'tooltip')
+  .style('opacity', 0);
+
+// SVG Container
+const svg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('class', 'graph')
+  .append('g')
+  .attr('transform', `translate(${margin.left},${margin.top})`);
