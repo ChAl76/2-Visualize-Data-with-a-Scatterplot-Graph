b2bb9b1b217fed96d3fb85667ca2886a3de28c7d
ChAl76 <chornyalexander@gmail.com>
2024-12-12 14:04:40 +0200

    init settings

---
+++ script.js
@@ -0,0 +1,6 @@
+const url =
+  'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+
+const margin = { top: 100, right: 40, bottom: 50, left: 60 };
+const width = 900 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
